04d552f7d423105546ccb54e27b8d0d91a27a395
Jordan Boggs <boggsjp@gmail.com>
2018-03-18 15:14:54 -0600

    Basic setup: serve index.html

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
-const path       = require('path');
+const logger     = require('morgan');
+const mongoose   = require('mongoose');
 
-const app                  = express();
-const PORT                 = process.env.PORT || 3000;
+// Scraping tools
+const axios = require('axios');
+const cheerio = require('cheerio');
 
+// Require all models
+const db = require('./models');
+
+const PORT = process.env.PORT || 3000;
+
+// Initialize express
+const app = express();
+
+// Config middleware
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling for submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve public folder as static directory
+app.use(express.static("public"));
+
+// Tell mongoose to return Promises
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/newsScraper");
+
+// Routes
+// Include all routes
+require('./routes/index')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
